845befa2e1eb69fdb139192d266f2d54a160add1
Diogo Amaral <diogoamaralazevedo@gmail.com>
2025-11-03 16:15:48 +0000

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from world.world import World
 
-w = World(width=5, height=5, mode="uniform", seed=None, gas_stations=1, warehouses=2, suppliers=3, stores=4, highway=True)
+w = World(width=5, height=5, mode="uniform", seed=None, gas_stations=0, warehouses=1, suppliers=0, stores=1, highway=False)
 for i in range(1, 11):
     w.tick(traffic_interval=1)
     print(f"Tick {i} completed.")
